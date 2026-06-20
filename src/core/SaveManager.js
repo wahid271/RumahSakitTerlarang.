5a90256f0efb0eefb168ad734d7f27e10db76e03
wahid271 <wahidtrade25@gmail.com>
2026-06-21 06:57:13 +0700

    Create SaveManager.js

---
+++ src/core/SaveManager.js
@@ -0,0 +1,21 @@
+/**
+ * SaveManager.js
+ * ----------------------------------
+ * Mengelola penyimpanan dan pembacaan progress game ke LocalStorage.
+ */
+
+// TODO: Implement Save System
+
+export class SaveManager {
+    constructor() {
+        // TODO: Initialize SaveManager
+    }
+
+    saveGame(data) {
+        // TODO: Save to LocalStorage
+    }
+
+    loadGame() {
+        // TODO: Load from LocalStorage
+    }
+}
