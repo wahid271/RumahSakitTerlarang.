f851e2ce386fdc6a6e706b9b1c2ff7b908628437
wahid271 <wahidtrade25@gmail.com>
2026-06-21 06:48:46 +0700

    Create AssetLoader.js

---
+++ src/core/AssetLoader.js
@@ -0,0 +1,21 @@
+/**
+ * AssetLoader.js
+ * ----------------------------------
+ * Melakukan preload terhadap semua aset (model, tekstur, audio) sebelum game dimulai.
+ */
+
+// TODO: Implement Asset Loading
+
+export class AssetLoader {
+    constructor() {
+        // TODO: Initialize loaders
+    }
+
+    async loadAll() {
+        // TODO: Load all assets
+    }
+
+    getAsset(name) {
+        // TODO: Return cached asset
+    }
+}
