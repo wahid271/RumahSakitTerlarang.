344a0fe000c95934d344ee06f5d5475722328ae2
wahid271 <wahidtrade25@gmail.com>
2026-06-21 06:47:08 +0700

    Create Game.js

---
+++ src/core/Game.js
@@ -0,0 +1,25 @@
+/**
+ * Game.js
+ * ----------------------------------
+ * Kelas utama untuk menginisialisasi dan menjalankan game loop.
+ */
+
+// TODO: Implement Game Loop and Initialization
+
+export class Game {
+    constructor() {
+        // TODO: Initialize core systems
+    }
+
+    start() {
+        // TODO: Start game loop
+    }
+
+    update() {
+        // TODO: Update logic
+    }
+
+    render() {
+        // TODO: Render scene
+    }
+}
