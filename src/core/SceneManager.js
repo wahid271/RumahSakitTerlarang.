783dc3ef2792c8661b4de3f5eb6267e361637be7
wahid271 <wahidtrade25@gmail.com>
2026-06-21 06:48:09 +0700

    Create SceneManager.js

---
+++ src/core/SceneManager.js
@@ -0,0 +1,21 @@
+/**
+ * SceneManager.js
+ * ----------------------------------
+ * Mengelola transisi, loading, dan unloading antar scene/level.
+ */
+
+// TODO: Implement Scene Management
+
+export class SceneManager {
+    constructor() {
+        // TODO: Initialize SceneManager
+    }
+
+    loadScene(sceneName) {
+        // TODO: Load scene
+    }
+
+    unloadScene() {
+        // TODO: Unload current scene
+    }
+}
