ded415cde62906df4d353675f1a01d1cf0ab1e30
wahid271 <wahidtrade25@gmail.com>
2026-06-21 06:59:25 +0700

    Create PlayerMovement.js

---
+++ src/player/PlayerMovement.js
@@ -0,0 +1,17 @@
+/**
+ * PlayerMovement.js
+ * ----------------------------------
+ * Menangani input WASD, sprint, jump, dan fisika dasar player.
+ */
+
+// TODO: Implement Player Movement
+
+export class PlayerMovement {
+    constructor() {
+        // TODO: Initialize movement
+    }
+
+    update() {
+        // TODO: Handle physics and input
+    }
+}
