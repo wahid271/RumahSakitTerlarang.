4b451ca9966233124c043cff241efa0f7abbd841
wahid271 <wahidtrade25@gmail.com>
2026-06-21 06:58:55 +0700

    Create Player.js

---
+++ src/player/Player.js
@@ -0,0 +1,17 @@
+/**
+ * Player.js
+ * ----------------------------------
+ * Entitas utama player. Menggabungkan semua komponen player.
+ */
+
+// TODO: Implement Player Entity
+
+export class Player {
+    constructor() {
+        // TODO: Initialize Player components
+    }
+
+    update() {
+        // TODO: Update player state
+    }
+}
