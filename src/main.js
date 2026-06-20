4bf3d9336d7cd8806a0ae27a8445588bc5979783
wahid271 <wahidtrade25@gmail.com>
2026-06-21 06:46:29 +0700

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,12 @@
+/**
+ * main.js
+ * ----------------------------------
+ * Entry point aplikasi. Menginisialisasi kelas Game dan memulai loop.
+ */
+
+import { Game } from './core/Game.js';
+
+// TODO: Implement Main Initialization
+
+const game = new Game();
+game.start();
