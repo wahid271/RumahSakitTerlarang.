33814b65b3a5c76795ce974d99cf7cc4009802a1
wahid271 <wahidtrade25@gmail.com>
2026-06-21 06:59:49 +0700

    Create PlayerCamera.js

---
+++ src/player/PlayerCamera.js
@@ -0,0 +1,17 @@
+/**
+ * PlayerCamera.js
+ * ----------------------------------
+ * Mengelola kamera first-person dan Pointer Lock Controls.
+ */
+
+// TODO: Implement First Person Camera
+
+export class PlayerCamera {
+    constructor() {
+        // TODO: Initialize camera and controls
+    }
+
+    update() {
+        // TODO: Update camera position/rotation
+    }
+}
