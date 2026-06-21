d47493d565bb1bfa7d37fbd1b771b23828db48c2
wahid271 <wahidtrade25@gmail.com>
2026-06-21 07:37:56 +0700

    Update PlayerStamina.js

--- src/player/PlayerStamina.js
+++ src/player/PlayerStamina.js
@@ -4,18 +4,42 @@
  * Mengelola stamina untuk sprint dan aksi lainnya.
  */
 
-// TODO: Implement Player Stamina
+import { MAX_STAMINA, STAMINA_REGEN_RATE, STAMINA_DRAIN_RATE } from '../utils/Constants.js';
 
 export class PlayerStamina {
-    constructor() {
-        // TODO: Initialize stamina
+    constructor(eventBus) {
+        this.eventBus = eventBus;
+        this.maxStamina = MAX_STAMINA;
+        this.currentStamina = MAX_STAMINA;
+    }
+
+    update(deltaTime, isSprinting) {
+        if (isSprinting) {
+            this.consume(STAMINA_DRAIN_RATE * deltaTime);
+        } else {
+            this.regenerate(STAMINA_REGEN_RATE * deltaTime);
+        }
     }
 
     consume(amount) {
-        // TODO: Reduce stamina
+        if (amount <= 0) return;
+        
+        this.currentStamina = Math.max(0, this.currentStamina - amount);
+        this.eventBus.publish('stamina:changed', { currentStamina: this.currentStamina });
     }
 
-    regenerate() {
-        // TODO: Recover stamina
+    regenerate(amount) {
+        if (amount <= 0) return;
+        
+        this.currentStamina = Math.min(this.maxStamina, this.currentStamina + amount);
+        this.eventBus.publish('stamina:changed', { currentStamina: this.currentStamina });
+    }
+
+    getStaminaPercentage() {
+        return (this.currentStamina / this.maxStamina) * 100;
+    }
+
+    hasStamina() {
+        return this.currentStamina > 0;
     }
 }
