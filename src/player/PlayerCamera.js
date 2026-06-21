4afa2f0cbc8edb3a35a33ef8276e6c5eda2c7f4d
wahid271 <wahidtrade25@gmail.com>
2026-06-21 07:36:36 +0700

    Update PlayerCamera.js

--- src/player/PlayerCamera.js
+++ src/player/PlayerCamera.js
@@ -4,14 +4,45 @@
  * Mengelola kamera first-person dan Pointer Lock Controls.
  */
 
-// TODO: Implement First Person Camera
+import * as THREE from 'three';
+import { PointerLockControls } from 'three/addons/controls/PointerLockControls.js';
 
 export class PlayerCamera {
-    constructor() {
-        // TODO: Initialize camera and controls
+    constructor(domElement) {
+        this.domElement = domElement;
+        
+        // Create camera
+        this.camera = new THREE.PerspectiveCamera(
+            75,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            1000
+        );
+        this.camera.position.set(0, 1.6, 5);
+        
+        // Setup pointer lock controls
+        this.controls = new PointerLockControls(this.camera, domElement);
+        
+        // Click to lock pointer
+        this.domElement.addEventListener('click', () => {
+            this.controls.lock();
+        });
+        
+        // Handle lock/unlock events
+        this.controls.addEventListener('lock', () => {
+            console.log('Pointer locked');
+        });
+        
+        this.controls.addEventListener('unlock', () => {
+            console.log('Pointer unlocked');
+        });
     }
 
     update() {
-        // TODO: Update camera position/rotation
+        // Camera is updated automatically by PointerLockControls
+    }
+
+    getCamera() {
+        return this.camera;
     }
 }
