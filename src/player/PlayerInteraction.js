f0f6f54fe6476f05d850a8c4ab6915362b5513a4
wahid271 <wahidtrade25@gmail.com>
2026-06-21 07:02:10 +0700

    Create PlayerInteraction.js

---
+++ src/player/PlayerInteraction.js
@@ -0,0 +1,17 @@
+/**
+ * PlayerInteraction.js
+ * ----------------------------------
+ * Raycasting untuk interaksi dengan objek lingkungan (pintu, item, dll).
+ */
+
+// TODO: Implement Interaction System
+
+export class PlayerInteraction {
+    constructor() {
+        // TODO: Initialize raycaster
+    }
+
+    checkInteraction() {
+        // TODO: Cast ray and find interactables
+    }
+}
