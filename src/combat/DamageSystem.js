425f8c3074142eab7b17667447ccc4dacd07bcc9
wahid271 <wahidtrade25@gmail.com>
2026-06-21 07:13:32 +0700

    Create DamageSystem.js

---
+++ src/combat/DamageSystem.js
@@ -0,0 +1,17 @@
+/**
+ * DamageSystem.js
+ * ----------------------------------
+ * Menghitung dan mengaplikasikan damage ke entitas yang terkena hit.
+ */
+
+// TODO: Implement Damage System
+
+export class DamageSystem {
+    constructor() {
+        // TODO: Initialize damage system
+    }
+
+    applyDamage(target, amount) {
+        // TODO: Apply damage to target
+    }
+}
