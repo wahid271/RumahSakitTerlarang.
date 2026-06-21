0a973f3bf40bd8527ec835147bb8430a4469f700
wahid271 <wahidtrade25@gmail.com>
2026-06-21 07:18:32 +0700

    Create ItemPickup.js

---
+++ src/inventory/ItemPickup.js
@@ -0,0 +1,17 @@
+/**
+ * ItemPickup.js
+ * ----------------------------------
+ * Merepresentasikan item fisik di dunia 3D yang bisa di-pickup oleh player.
+ */
+
+// TODO: Implement Item Pickup Entity
+
+export class ItemPickup {
+    constructor() {
+        // TODO: Initialize 3D pickup object
+    }
+
+    pickup() {
+        // TODO: Handle pickup logic
+    }
+}
