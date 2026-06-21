6270ff168a7db57520983f0b4946c8aa491c52a6
wahid271 <wahidtrade25@gmail.com>
2026-06-21 07:13:01 +0700

    Create HitDetection.js

---
+++ src/combat/HitDetection.js
@@ -0,0 +1,17 @@
+/**
+ * HitDetection.js
+ * ----------------------------------
+ * Melakukan raycast/boxcast untuk mendeteksi tabrakan senjata dengan musuh.
+ */
+
+// TODO: Implement Hit Detection
+
+export class HitDetection {
+    constructor() {
+        // TODO: Initialize hit detection
+    }
+
+    checkHit() {
+        // TODO: Perform raycast for melee hit
+    }
+}
