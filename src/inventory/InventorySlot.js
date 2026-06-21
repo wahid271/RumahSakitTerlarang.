7c53fb2ca65e940e9c344d97b556b573cb11556c
wahid271 <wahidtrade25@gmail.com>
2026-06-21 07:16:49 +0700

    Create InventorySlot.js

---
+++ src/inventory/InventorySlot.js
@@ -0,0 +1,17 @@
+/**
+ * InventorySlot.js
+ * ----------------------------------
+ * Merepresentasikan satu slot di dalam UI inventaris.
+ */
+
+// TODO: Implement Inventory Slot
+
+export class InventorySlot {
+    constructor() {
+        // TODO: Initialize slot
+    }
+
+    setItem(item) {
+        // TODO: Set item in slot
+    }
+}
