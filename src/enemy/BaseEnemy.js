7514e9c02e3a68977583bec301db427740942a8e
wahid271 <wahidtrade25@gmail.com>
2026-06-21 07:04:08 +0700

    Update BaseEnemy.js

--- src/enemy/BaseEnemy.js
+++ src/enemy/BaseEnemy.js
@@ -1,1 +1,21 @@
+/**
+ * BaseEnemy.js
+ * ----------------------------------
+ * Kelas abstrak dasar untuk semua musuh. Mendefinisikan state machine.
+ */
 
+// TODO: Implement Base Enemy
+
+export class BaseEnemy {
+    constructor() {
+        // TODO: Initialize base properties
+    }
+
+    update() {
+        // TODO: Update AI state
+    }
+
+    takeDamage(amount) {
+        // TODO: Apply damage
+    }
+}
