a5d049572209c0c17a617cf950ad3ed1100eb4ee
wahid271 <wahidtrade25@gmail.com>
2026-06-21 07:15:43 +0700

    Create Inventory.js

---
+++ src/inventory/Inventory.js
@@ -0,0 +1,21 @@
+/**
+ * Inventory.js
+ * ----------------------------------
+ * Logika utama inventaris player, menambah/menghapus item.
+ */
+
+// TODO: Implement Inventory Logic
+
+export class Inventory {
+    constructor() {
+        // TODO: Initialize inventory array
+    }
+
+    addItem(item) {
+        // TODO: Add item to inventory
+    }
+
+    removeItem(itemId) {
+        // TODO: Remove item
+    }
+}
