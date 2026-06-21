03ed3d1679e028c3eb52aa43b460f0c08c6a8f9d
wahid271 <wahidtrade25@gmail.com>
2026-06-21 07:17:20 +0700

    Create Item.js

---
+++ src/inventory/Item.js
@@ -0,0 +1,17 @@
+/**
+ * Item.js
+ * ----------------------------------
+ * Kelas dasar untuk semua item yang bisa dimasukkan ke inventaris.
+ */
+
+// TODO: Implement Base Item
+
+export class Item {
+    constructor(id, name) {
+        // TODO: Initialize item properties
+    }
+
+    use() {
+        // TODO: Implement item usage
+    }
+}
