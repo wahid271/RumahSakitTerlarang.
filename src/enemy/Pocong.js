3f403dbb64c1cb99b5dde1e718ffb2237e624690
wahid271 <wahidtrade25@gmail.com>
2026-06-21 07:07:53 +0700

    Create Pocong.js

---
+++ src/enemy/Pocong.js
@@ -0,0 +1,25 @@
+/**
+ * Pocong.js
+ * ----------------------------------
+ * Implementasi spesifik AI dan behavior untuk hantu Pocong.
+ * Inherit dari BaseEnemy.
+ */
+
+import { BaseEnemy } from './BaseEnemy.js';
+
+// TODO: Implement Pocong AI
+
+export class Pocong extends BaseEnemy {
+    constructor() {
+        super();
+        // TODO: Initialize Pocong specific stats
+    }
+
+    patrol() {
+        // TODO: Implement patrol behavior
+    }
+
+    chase() {
+        // TODO: Implement chase behavior
+    }
+}
