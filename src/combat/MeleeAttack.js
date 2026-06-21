63c88b6d355950c3a373a930c580a17021791d0d
wahid271 <wahidtrade25@gmail.com>
2026-06-21 07:12:31 +0700

    Create MeleeAttack.js

---
+++ src/combat/MeleeAttack.js
@@ -0,0 +1,17 @@
+/**
+ * MeleeAttack.js
+ * ----------------------------------
+ * Menangani logika serangan melee, cooldown, dan trigger animasi.
+ */
+
+// TODO: Implement Melee Attack
+
+export class MeleeAttack {
+    constructor() {
+        // TODO: Initialize attack logic
+    }
+
+    attack() {
+        // TODO: Trigger attack animation and cooldown
+    }
+}
