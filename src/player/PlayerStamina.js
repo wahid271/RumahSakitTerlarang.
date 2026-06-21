264060d59373c996502f55f5957f903efe8c7acb
wahid271 <wahidtrade25@gmail.com>
2026-06-21 07:01:24 +0700

    Create PlayerStamina.js

---
+++ src/player/PlayerStamina.js
@@ -0,0 +1,21 @@
+/**
+ * PlayerStamina.js
+ * ----------------------------------
+ * Mengelola stamina untuk sprint dan aksi lainnya.
+ */
+
+// TODO: Implement Player Stamina
+
+export class PlayerStamina {
+    constructor() {
+        // TODO: Initialize stamina
+    }
+
+    consume(amount) {
+        // TODO: Reduce stamina
+    }
+
+    regenerate() {
+        // TODO: Recover stamina
+    }
+}
