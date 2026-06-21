2c3539b5316e12f61c2b73f36eff506ae21c339d
wahid271 <wahidtrade25@gmail.com>
2026-06-21 07:08:25 +0700

    Create WeaponSystem.js

---
+++ src/src/combat/WeaponSystem.js
@@ -0,0 +1,17 @@
+/**
+ * WeaponSystem.js
+ * ----------------------------------
+ * Mengelola senjata yang sedang dipegang, switching senjata, dan animasi.
+ */
+
+// TODO: Implement Weapon System
+
+export class WeaponSystem {
+    constructor() {
+        // TODO: Initialize weapon system
+    }
+
+    equipWeapon(weaponId) {
+        // TODO: Equip weapon
+    }
+}
