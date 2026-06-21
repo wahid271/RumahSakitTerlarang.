631d17dd854ea94fe4bb64116fe71bbdc7d5f488
wahid271 <wahidtrade25@gmail.com>
2026-06-21 07:06:18 +0700

    Create EnemyManager.js

---
+++ src/enemy/EnemyManager.js
@@ -0,0 +1,21 @@
+/**
+ * EnemyManager.js
+ * ----------------------------------
+ * Mengelola spawning, update, dan despawning semua musuh di level saat ini.
+ */
+
+// TODO: Implement Enemy Manager
+
+export class EnemyManager {
+    constructor() {
+        // TODO: Initialize manager
+    }
+
+    spawnEnemy(type, position) {
+        // TODO: Create and add enemy
+    }
+
+    updateAll() {
+        // TODO: Update all active enemies
+    }
+}
